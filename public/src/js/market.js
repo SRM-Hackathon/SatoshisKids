0cfdc8bdf01e315dc57625152736ab4a4a8a7c7b
Ankit Kishore <ankitkishore_aw@srmuniv.edu.in>
2018-10-12 20:11:58 +0530

    created a new form js

---
+++ public/src/js/market.js
@@ -0,0 +1,8 @@
+
+var messagesRef = firebase.database().ref('image');
+
+messagesRef.on("child_added", snap=>{
+
+    var name = snap.child("src").val();
+    document.getElementById('image').src=name;
+  })
